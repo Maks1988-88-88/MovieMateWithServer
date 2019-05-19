f01a3bf778b152b5cd6614b8b0f6447ace0258e8
Maksim Tsopa <maks98242@gmail.com>
2019-05-19 12:16:00 +0300

    add Api img, slice size descr 100

--- src/components/AppBody/index.jsx
+++ src/components/AppBody/index.jsx
@@ -25,51 +25,52 @@ class AppBody extends React.Component {
   //   });
   // };
 
-  // componentWillMount() {
-  //   fetch("https://api.themoviedb.org/3/search/movie?api_key=680d1c453082be4c031bb516ed6208df&page=1&query=1&include_adult=false", {
-  //     headers: new Headers({
-  //       Authorization:
-  //         "563492ad6f91700001000001972aa481984848e1a57e5f8fa48c679d"
-  //     })
-  //   })
-  //     .then(response => {
-  //       if (response.ok) {
-  //         return response.json();
-  //       }
-  //       throw new Error("Error" + response.statusText);
-  //     })
-  //     .then(data => {
-  //       const films = data.results.map(film => ({
-  //         id: film.id,
-  //         release_date: film.release_date,
-  //         descr: film.overview,
-  //         tittle: film.title,
-  //         rating: film.vote_average,
-  //         // backdrop_path: photo.backdrop_path,
-  //         //poster_path: film.poster_path
-  //       }));
-
-  //       console.log(films);
-
-  //       // this.setState({
-  //       //   post: films
-  //       // });
-  //     })
-  //     // console.log(data))
-  //     .catch(err => console.error(err));
-  // }
-
-  componentWillMount(){
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=680d1c453082be4c031bb516ed6208df&page=1&query=1&include_adult=false`)
+  componentWillMount() {
+    fetch("https://api.themoviedb.org/3/search/movie?api_key=680d1c453082be4c031bb516ed6208df&page=1&query=1&include_adult=false", {
+      headers: new Headers({
+        Authorization:
+          "563492ad6f91700001000001972aa481984848e1a57e5f8fa48c679d"
+      })
+    })
       .then(response => {
         if (response.ok) {
           return response.json();
         }
         throw new Error("Error" + response.statusText);
       })
-    .then(data => console.log(data))
-    .catch (err => console.error(err));
+      .then(data => {
+        const films = data.results.map(film => ({
+          id: film.id,
+          release_date: film.release_date,
+          descr: film.overview.slice(0, 100) + "...",
+          tittle: film.title,
+          rating: film.vote_average,
+          poster_path:
+            "https://image.tmdb.org/t/p/w300_and_h450_bestv2" +
+            film.poster_path
+        }));
+
+        // console.log(films[0].descr.slice(0, 100) + '...');
+
+        this.setState({
+          post: films
+        });
+      })
+      // console.log(data))
+      .catch(err => console.error(err));
   }
+
+  // componentWillMount(){
+  //   fetch(`https://api.themoviedb.org/3/search/movie?api_key=680d1c453082be4c031bb516ed6208df&page=1&query=1&include_adult=false`)
+  //     .then(response => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       }
+  //       throw new Error("Error" + response.statusText);
+  //     })
+  //   .then(data => console.log(data))
+  //   .catch (err => console.error(err));
+  // }
 
   // poster: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2' + movie.poster_path
  // API Key(v3 auth) 680d1c453082be4c031bb516ed6208df
@@ -93,11 +94,12 @@ class AppBody extends React.Component {
         <MovieCardCollection>
           {post.map(post => (
             <MovieCard
-              img={post.img}
+              img={post.poster_path}
               tittle={post.tittle}
               descr={post.descr}
               rating={post.rating}
               key={post.id}
+              release_date={post.release_date}
               onClick={() => {
                 this.onDeleteCard(post.id);
               }}
--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -5,21 +5,24 @@ import PropTypes from "prop-types";
 
 class MovieCard extends React.Component {
   render() {
-    const { tittle, descr, rating, onClick, img } = this.props;
+    const {
+      tittle,
+      descr,
+      rating,
+      onClick,
+      img,
+      release_date
+    } = this.props;
     return (
       <div className="MovieCard">
-
         <div>
           <span className="MovieInfo__rating">{rating}</span>
-          <img src={img} alt="img" className='MovieCard__poster'/>
+          <img src={img} alt="img" className="MovieCard__poster" />
           <h2 className="MovieInfo__title">{tittle}</h2>
           <p className="MovieInfo__descr">{descr} </p>
-          <p className='MovieInfo__release'>Release date: 2019</p>
+          <p className="MovieInfo__release">Release date: {release_date}</p>
         </div>
-        <button
-          className="MovieCard__btn "
-          onClick={onClick}
-        >
+        <button className="MovieCard__btn " onClick={onClick}>
           +
         </button>
       </div>
@@ -30,7 +33,7 @@ class MovieCard extends React.Component {
 MovieCard.propTypes = {
   tittle: PropTypes.string.isRequired,
   descr: PropTypes.string.isRequired,
-  rating: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired
 };
 
 export default MovieCard;
