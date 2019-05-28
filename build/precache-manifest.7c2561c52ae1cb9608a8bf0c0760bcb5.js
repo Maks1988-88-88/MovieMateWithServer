4c5a6c2cdf9e4a70c2fed3a70708d6e0eb7c25b5
Maksim Tsopa <maks98242@gmail.com>
2019-05-28 21:32:51 +0300

    home page

---
+++ build/precache-manifest.7c2561c52ae1cb9608a8bf0c0760bcb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9110377c5291c71b9802",
+    "url": "/MovieMateWithServer/build/static/js/runtime~main.9110377c.js"
+  },
+  {
+    "revision": "4db202ac5f3249c6a020",
+    "url": "/MovieMateWithServer/build/static/js/main.4db202ac.chunk.js"
+  },
+  {
+    "revision": "baaf5e9c1a07555c907f",
+    "url": "/MovieMateWithServer/build/static/js/1.baaf5e9c.chunk.js"
+  },
+  {
+    "revision": "4db202ac5f3249c6a020",
+    "url": "/MovieMateWithServer/build/static/css/main.768b7569.chunk.css"
+  },
+  {
+    "revision": "5decf8dbc9124064e3d401f162f9160d",
+    "url": "/MovieMateWithServer/build/index.html"
+  }
+];
